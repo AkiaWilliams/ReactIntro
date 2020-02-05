2bf5a9d3bf784cd9569d9064bc59a619a4995b7a
Dcouncildev <dwightcouncil728300@gmail.com>
2020-02-05 20:35:39 +0000

    init test

--- src/js/component/Footer.js
+++ src/js/component/Footer.js
@@ -1,17 +1,12 @@
 import React from "react";
 
 const Footer = () => {
-  return (
-<div>  
-  <div className="card-body">
-    <h5 className="card-title">Special title treatment</h5>
-    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+  return (  
+     <div>
+      <div style={{height:'60%',background:'black'}}>
+        <h1> I amm Footer </h1>            
   </div>
-  <div className="card-footer text-muted">
-    2 days ago
-  </div>
-  </div>
+</div>
   );
 };
 
